cefcd7df5c6a3da406d7518a63c22659f94b4be3
Aniruddh <adubge@gmail.com>
2023-09-24 20:33:09 +0530

    a

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,23 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-
+import { ThirdwebProvider, ThirdwebSDKProvider } from "@thirdweb-dev/react";
+import { ethers } from "ethers";
 import "~/styles/globals.css";
 
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (<>
+    <ThirdwebProvider activeChain="mumbai" clientId="af574d551a74949aacbf76fbee4f40f4">
+    <ThirdwebSDKProvider
+      activeChain={"mumbai"}
+      // Example: Use ethers to get the signer from the window.ethereum object
+      signer={new ethers.providers.Web3Provider(window.ethereum).getSigner()}
+      clientId="af574d551a74949aacbf76fbee4f40f4"
+    >
+      <Component {...pageProps} />;
+      </ThirdwebSDKProvider>
+    </ThirdwebProvider>
+  </>)
 };
 
 export default MyApp;
+//format
