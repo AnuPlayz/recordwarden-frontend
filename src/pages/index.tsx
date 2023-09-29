5d96c5c74f2608bab67d1b1f8afafc27c2a7a5ba
Aniruddh <adubge@gmail.com>
2023-09-30 01:37:26 +0530

    index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="text-2xl text-white text-center">
-        <h1 className="mb-4 text-lg font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Security with <span class="text-blue-600 dark:text-blue-500">the world's #1</span> Decentralized eVault.</h1>
+      <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">RecordWarden</span> eVault</h1>
         <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">Here at RecordWarden we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
         <Link href="/cases" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
           Learn more
