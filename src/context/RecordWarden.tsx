2b2d25d402cd09973e2f1fec2dbced73ee1a516d
Aniruddh <adubge@gmail.com>
2023-09-30 13:15:20 +0530

    e

--- src/context/RecordWarden.tsx
+++ src/context/RecordWarden.tsx
@@ -1,25 +1,30 @@
 'use client'
-import { useContract } from "@thirdweb-dev/react";
+import { useContract, useSigner } from "@thirdweb-dev/react";
 import { createContext, useEffect } from "react";
 import { CONTRACT_ADDRESS } from "~/config";
 import Data from "~/RecordWarden.json"
 
 export const RecordWardenContext = createContext<{
   contract: any; // Replace 'any' with the actual contract type
+  signer: any
 }>({
   contract: null, // Provide an initial value for contract
+  signer: null
 });
 
 export const RecordWardenProvider = (props: any) => {
+  const signer = useSigner()
   const { contract } = useContract(CONTRACT_ADDRESS, Data.abi);
 
   useEffect(() => {
     //@ts-ignore 
     if (window) window.contract = contract
+    //@ts-ignore
+    if (window) window.signer = signer
   }, [contract])
 
   return (
-    <RecordWardenContext.Provider value={{ contract }}>
+    <RecordWardenContext.Provider value={{ contract, signer }}>
       {props.children}
     </RecordWardenContext.Provider>
   );
